b27c85e3250a2dd4358277277d96f6ed381143cf
Noel Varghese Oommen <53402523+noelvargheseoommen@users.noreply.github.com>
2023-03-17 19:48:50 +0530

    reorder

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
 
       <Stack spacing={800} direction='row' align='center'>
 
-      <Image height='90px' src='.\src\assets\logo2.jpeg' alt='c02 icon' />
+      <Image height='90px' src='.\src\assets\images\logo2.jpeg' alt='c02 icon' />
 
       <Stack spacing={4} direction='row' align='center'>
 
@@ -73,7 +73,7 @@ function App() {
 <br></br> <br></br>
 <br></br><br></br>
 <br></br>
-  <Image height='200px' src='.\src\assets\carbon-neutral.png' alt='carbon neutral icon' />
+  <Image height='200px' src='.\src\assets\images\carbon-neutral.png' alt='carbon neutral icon' />
   <br></br><br></br>
   <h2 >Enter the world of</h2> <h22 >[carbon]</h22><h2>swap and help</h2>
   <Spacer /><br></br>
@@ -85,7 +85,7 @@ function App() {
         </button>
         <br></br> <br></br><br></br>
         <h1 color='#C9E265' >6,868,690,067   </h1>
-        <Image height='60px' src='.\src\assets\co2.png' alt='c02 icon' />
+        <Image height='60px' src='.\src\assets\images\co2.png' alt='c02 icon' />
         <Spacer /><br></br>
         <h2>Liquidity locked on the </h2><h22>[carbon]</h22> <h2>swap</h2>
         <br></br> <br></br> <br></br> <br></br><br></br><br></br>
