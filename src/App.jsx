8fc63c82a7d1b46d8588638541981f2dff6f93f2
Noel Varghese Oommen <53402523+noelvargheseoommen@users.noreply.github.com>
2023-03-20 18:47:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,6 @@ import TopNav from './components/TopNav'
 import Home from './pages/Home'
 import BottomNav from './components/BottomNav'
 
-import {
-  ChakraProvider,
-  Box,
-  Button,
-  ButtonGroup,
-  Spacer,
-  Image, Flex,  HStack , chakra , Stack,
-
-} from '@chakra-ui/react'
-
-
 
 function App() {
 
