09ef59d6eca548756aecf557dba07828bd4bc411
Noel Varghese Oommen <53402523+noelvargheseoommen@users.noreply.github.com>
2023-03-29 09:34:33 +0530

    menu

---
+++ src/components/LiquidityMenu/index.jsx
@@ -0,0 +1,36 @@
+import {
+ 
+    Box, Center, extendTheme, Flex, Stack, HStack,VStack, SimpleGrid, Text, Input, Select, Button,
+   
+  } from '@chakra-ui/react'
+
+
+
+
+
+
+function LiquidityMenu() {
+
+
+    return (
+
+<div>
+
+<Box>
+  <Stack>
+    <Button>
+
+    </Button>
+
+    <Button>
+      
+    </Button>
+
+  </Stack>
+</Box>
+
+</div>
+)
+}
+
+export default LiquidityMenu
--- src/pages/liquidity/index.jsx
+++ src/pages/liquidity/index.jsx
@@ -3,17 +3,17 @@
 import TopNav from '../../components/TopNav'
 import BottomNav from '../../components/BottomNav'
 import AddToPool from '../../components/AddToPool'
+import LiquidityMenu from '../../components/LiquidityMenu'
 
 
 import {
     ChakraProvider,
     Box,
-    Button,
-    ButtonGroup,
-    Spacer,
+    Button, Spacer,
     Image, Flex,  HStack , chakra , Stack, Center, VStack,Text,
   
   } from '@chakra-ui/react'
+
 
 
 
@@ -24,10 +24,23 @@ function Liquidity() {
 return (
 
   <div className="home">
+
+
  
 <TopNav/>
 
+
+
+<Flex p='4'>
+
+<LiquidityMenu/>
+
+
+<Box bg='transparent' height='1000px'></Box>
+
+
 <AddToPool/>
+</Flex>
 
 <BottomNav/>
 
