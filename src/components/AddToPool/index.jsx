70e73af369c7fc463c2773a657b3122d501104de
Noel Varghese Oommen <53402523+noelvargheseoommen@users.noreply.github.com>
2023-03-29 16:14:24 +0530

    new

--- src/components/AddToPool/index.jsx
+++ src/components/AddToPool/index.jsx
@@ -20,7 +20,7 @@ function AddToPool() {
 
 <SimpleGrid columns={1} spacing={10}>
 
-<Box bg='transparent' height='60px'></Box>
+<Box bg='transparent' height='20px'></Box>
 
 <Center>
 <Flex >
@@ -32,62 +32,64 @@ function AddToPool() {
 
 
 <Center>
-<Box bg='brand.200' height='520px' w='1183px' borderRadius='3xl'  boxShadow='inner' p='4'>
+<Box bg='brand.300' height='560px' w='1183px' borderRadius='3xl'  boxShadow='inner-lg' p='4'>
 
 <VStack  spacing={8} >
 
+<Box bg='transparent' height='5px'  ></Box>
 
-<Box bg='transparent' height='20px'  ></Box>
-
-
-<Flex>
-
-<Select placeholder='Select Token' borderRadius='2xl' bg='white'  variant='filled' w='450px' h='76px' fontSize='20px'  textColor='black' >
+<Box  boxShadow='dark-lg' borderRadius='2xl'>
+<Select placeholder='Select Token' textAlign='Center' borderRadius='2xl' bg='white'  variant='filled' w='350px' h='76px' fontSize='25px'  textColor='black'  >
   <option value='BCT'>BCT</option>
   <option value='NCT'>NCT</option>
   <option value='MCO2'>MCO2</option>
 </Select>
+</Box>
 
-<Box bg='transparent' w='20px'  ></Box>
+<Box bg='transparent' height='1px'  ></Box>
 
-<Select placeholder='Select Token' borderRadius='2xl' bg='white'  variant='filled' w='450px' h='76px' fontSize='20px'  textColor='black'>
-  <option value='BCT'>BCT</option>
-  <option value='NCT'>NCT</option>
-  <option value='MCO2'>MCO2</option>
-</Select>
-
-</Flex>
 
 <Flex>
-  <Button variant='solid' borderRadius='2xl' w='450px' h='76px' bg='transparent' borderColor="black" fontSize='20px'>
+<Box   boxShadow='inner-dark-lg' borderRadius='3xl'>
+  <Button variant='solid' borderRadius='2xl' w='450px' h='76px' bg='black' borderColor="black" fontSize='25px' textColor='white'>
       Add Liquidity
   </Button>
+</Box>
+  <Box bg='transparent' w='150px'  ></Box>
 
-  <Box bg='transparent' w='20px'  ></Box>
-
-  <Button  variant='solid' borderRadius='2xl' w='450px' h='76px' bg='transparent' borderColor="black" fontSize='20px'>
+  <Box  boxShadow='inner-dark-lg' borderRadius='3xl'>
+  <Button  variant='solid' borderRadius='2xl' w='450px' h='76px' bg='black' borderColor="black" fontSize='25px'textColor='white'>
       Remove Liquidity
   </Button>
+  </Box>
 </Flex>
 
 <Flex>
+<Box  boxShadow='dark-lg' borderRadius='3xl'>
 <Input placeholder='0.0' textAlign='right' borderRadius='2xl' bg='lblack.100'  variant='filled' w='450px' h='76px' fontSize='35px'  textColor='grey'/>
+</Box>
 
-<Box bg='transparent' w='20px'  ></Box>
+<Box bg='transparent' w='150px'  ></Box>
 
+<Box  boxShadow='dark-lg' borderRadius='3xl'>
 <Input placeholder='0.0' textAlign='right' borderRadius='2xl' bg='lblack.100'  variant='filled' w='450px' h='76px' fontSize='35px' textColor='white'/>
+</Box>
 </Flex>
 
 <Flex>
+<Box  boxShadow='dark-lg' borderRadius='3xl'>
 <Button bg='Green' variant='solid' w='450px' h='76px' fontSize='35px' borderRadius='2xl' textColor='white'>
     Deposit
 </Button>
+</Box>
 
-<Box bg='transparent' w='20px'  ></Box>
+<Box bg='transparent' w='150px'  ></Box>
 
+<Box  boxShadow='dark-lg' borderRadius='3xl'>
 <Button bg='maroon' variant='solid'  w='450px' h='76px' fontSize='35px' borderRadius='2xl' textColor='white' >
     Withdraw
 </Button>
+</Box>
 
 </Flex>
 
@@ -96,7 +98,7 @@ function AddToPool() {
 </Center>
 
 
-<Box bg='transparent' height='110px'></Box>
+<Box bg='transparent' height='80px'></Box>
  
  
  </SimpleGrid>
--- src/pages/liquidity/index.jsx
+++ src/pages/liquidity/index.jsx
@@ -3,14 +3,11 @@
 import TopNav from '../../components/TopNav'
 import BottomNav from '../../components/BottomNav'
 import AddToPool from '../../components/AddToPool'
-import LiquidityMenu from '../../components/LiquidityMenu'
 
 
 import {
+  
     ChakraProvider,
-    Box,
-    Button, Spacer,
-    Image, Flex,  HStack , chakra , Stack, Center, VStack,Text,
   
   } from '@chakra-ui/react'
 
@@ -30,17 +27,8 @@ return (
 <TopNav/>
 
 
-
-<Flex p='4'>
-
-<LiquidityMenu/>
-
-
-<Box bg='transparent' height='1000px'></Box>
-
-
 <AddToPool/>
-</Flex>
+
 
 <BottomNav/>
 
