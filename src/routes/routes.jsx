42fe34710c727e6b15a8af61e8d6709e31bee6e1
Noel Varghese Oommen <53402523+noelvargheseoommen@users.noreply.github.com>
2023-06-25 09:33:40 +0530

    removed dash

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,9 +4,7 @@ import Swap from "../pages/swap/Swap";
 import DashProfile from "../pages/Dashboard/DashProfile";
 import About from "../pages/about";
 import Liquidity from "../pages/liquidity";
-import DashViewVehicles from "../pages/Dashboard/DashViewVehicles";
 import DashAddVehicles from "../pages/Dashboard/DashAddVehicles";
-import DashHistory from "../pages/Dashboard/DashHistory";
 
 // Add your routes here.
 // Use Link element from react-router to navigate to specific url. --see src/pages/Home/index.jsx (Swap Link)
@@ -39,19 +37,10 @@ const router = createBrowserRouter([
       },
 
       {
-        path: "/dashviewvehicles", // Url to render the component in element property.
-        element: <DashViewVehicles />,
-      },
-
-      {
         path: "/dashaddvehicles", // Url to render the component in element property.
         element: <DashAddVehicles />,
       },
 
-      {
-        path: "/dashhistory", // Url to render the component in element property.
-        element: <DashHistory />,
-      },
     ],
   },
 ]);
